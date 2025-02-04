bc54a18c342d5644e3a4336d64ab964b5ddb93ec
BOSS1480 <yboss1480@gmail.com>
2025-02-04 23:50:29 +0200

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -2,12 +2,13 @@
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = 'Repo'
-    ST_BN1_URL = 'https://www.github.com/VJBots/WZML-X'
-    ST_BN2_NAME = 'Updates'
-    ST_BN2_URL = 'https://t.me/VJ_Botz'
-    ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
-<b>Type {help_command} to get a list of available commands</b>'''
+    ST_BN1_NAME = 'למעבר לקבוצה'
+    ST_BN1_URL = 'https://t.me/+oVKj6OBTxBAwYjM0'
+    ST_BN2_NAME = ''
+    ST_BN2_URL = ''
+    ST_MSG = '''<i>הבוט הזה יכול לשכפל את כל הקישורים|קבצים|טורנטים שלך ל-Google Drive או לכל שירות אחסון בענן באמצעות rclone או ל-Telegram או לשרתים של DDL.</i>
+
+<b>שלח {help_command} כדי לראות את הרשימת פקודות</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
     ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>'''
     OWN_TOKEN_GENERATE = '''<b>Temporary Token is not yours!</b>\n\n<i>Kindly generate your own.</i>'''
